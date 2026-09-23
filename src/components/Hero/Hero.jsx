f2e815cb64c2fc02a0317fb33ed1c6b4cdf0b175
AnnVynnychenkoa <avinnichenko93@gmail.com>
2026-09-23 14:11:48 +0200

    feat: navigate to Nannies page on Get Started button click

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,14 +6,26 @@ import {
   GetStartedBtn,
   StatsContainer,
 } from './Hero.styled';
+import { useNavigate } from 'react-router-dom';
 
 function Hero() {
+  const navigateOnNanniesPage = useNavigate();
+
+  function handleGetStartedClick() {
+    navigateOnNanniesPage('/nannies');
+  }
+
   return (
     <HeroSection>
       <HeroContainer>
         <h1>Make Life Easier for the Family:</h1>
         <p>Find Babysitters Online for All Occasions</p>
-        <GetStartedBtn title="Get started" paddingX={46} paddingY={16}>
+        <GetStartedBtn
+          title="Get started"
+          paddingX={46}
+          paddingY={16}
+          onClick={handleGetStartedClick}
+        >
           <ArrowIcon icon="ci:arrow-up-right-lg" />
         </GetStartedBtn>
       </HeroContainer>
